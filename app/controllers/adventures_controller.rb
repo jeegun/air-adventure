e4e94540c1be8385e9791748bc59354897c23b9b
Tamas Papp <toma.papp92@gmail.com>
2020-03-17 11:52:01 +0100

    Create method and view for index created

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,11 +1,29 @@
 class AdventuresController < ApplicationController
 
+  before_action :set_adventure, only: [:show]
+
   def index
     @adventures = Adventure.all
   end
 
   def show
+  end
+
+  def new
+    @adventure = Adventure.new
+  end
+
+  def create
+    @adventure = Adventure.new(adventure_params)
+  end
+
+  private
+
+  def set_adventure
     @adventure = Adventure.find(params[:id])
   end
 
+  def adventure_params
+    params.require(:adventure).permit(:name, :description, :location, :price, :languages, :number_of_guests)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-  get 'adventures', to: 'adventures#index', as: :adventures
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

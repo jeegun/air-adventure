a5fc4d81afafee2f600a74bfb192d619304e5b50
Marco de Biolley <marcdebiolley@gmail.com>
2020-03-17 11:57:32 +0100

    added validation

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -2,4 +2,10 @@ class Adventure < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
+  validates :number_of_guests, presence: true
+  validates :location, presence: true
+  validates :price, presence: true
+  validates :languages, presence: true
+  validates :description, presence: true
+  validates :name, presence: true, uniqueness: true
 end

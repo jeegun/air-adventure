ad4b70cbb7268afc1f3e61cb3ae875fd8ecf1559
Jee Gun Kim <jeegun@gmail.com>
2020-03-19 07:11:13 -0700

    fixed the bookings form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,15 +1,16 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :adventure
-  validates :start_date, :end_date, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
   validates :nr_of_booked_guests, presence: true
   validate :end_date_after_start_date
   validate :booked_guests_less_than_max
-
-  private
+  before_create :calculate_total_price
 
   def end_date_after_start_date
-    if end_date < start_date
+    return if end_date.blank? || start_date.blank?
+    if self.end_date < self.start_date
       errors.add(:end_date, "must be after the start date")
     end
   end
@@ -18,5 +19,12 @@ class Booking < ApplicationRecord
     if nr_of_booked_guests > adventure.number_of_guests
       errors.add(:nr_of_booked_guests, "cannot be more than max")
     end
+  end
+
+  def calculate_total_price
+    unit_price = self.adventure.price
+    nr_of_days = self.end_date.day - self.start_date.day
+    nr_of_booked_guests = self.nr_of_booked_guests
+    self.total_price = unit_price * nr_of_days * nr_of_booked_guests
   end
 end

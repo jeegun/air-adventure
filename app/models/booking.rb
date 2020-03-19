6ca57e8a8eb19ed5653b2bd6f1597fb67083ea99
Jee Gun Kim <jeegun@gmail.com>
2020-03-19 06:24:35 -0700

    removed a return i didn't understand

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,15 +4,19 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true
   validates :nr_of_booked_guests, presence: true
   validate :end_date_after_start_date
+  validate :booked_guests_less_than_max
 
   private
 
   def end_date_after_start_date
-    return if end_date.blank? || start_date.blank?
-
-      if end_date < start_date
-        errors.add(:end_date, "must be after the start date")
-      end
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
     end
   end
 
+  def booked_guests_less_than_max
+    if nr_of_booked_guests > adventure.number_of_guests
+      errors.add(:nr_of_booked_guests, "cannot be more than max")
+    end
+  end
+end
